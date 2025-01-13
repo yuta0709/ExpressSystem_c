a221ba1c40992f7aad3df16c7b305d1926652b97
yuta0709 <yuta0709dev@gmail.com>
2025-01-14 00:58:59 +0900

    状態変更のロジックを修正

--- TransportVehicle/controller/CollectturnController.c
+++ TransportVehicle/controller/CollectturnController.c
@@ -11,13 +11,44 @@
 #include "CollectturnController.h"
 #include "ActionController.h"
 
+static void CTC_entry(void);
+static void CTC_do_action(void);
+static void CTC_exit(void);
+
 /**
  * 収集後反転制御
  */
 void CTC_action(void) {
+    if (pre_state != AC_STATUS_COLLECTTURN) {
+        CTC_entry();
+        pre_state = AC_STATUS_COLLECTTURN;
+    }
+
+    CTC_do_action();
+}
+
+/**
+ * 入口処理
+ */
+static void CTC_entry(void) {
+    // 必要に応じて入口処理を実装
+}
+
+/**
+ * 実処理
+ */
+static void CTC_do_action(void) {
     // 右に反転する
     TRC_do_turnright();
     
     // 状態を配達中に移す
+    CTC_exit();
     cur_state = AC_STATUS_DERIVER;
+}
+
+/**
+ * 出口処理
+ */
+static void CTC_exit(void) {
+    // 必要に応じて出口処理を実装
 } 
--- TransportVehicle/controller/DeriverController.c
+++ TransportVehicle/controller/DeriverController.c
@@ -14,10 +14,33 @@
 // 配達先の色を示す変数の定義
 int target_color = 0;
 
+static void DC_entry(void);
+static void DC_do_action(void);
+static void DC_exit(void);
+
 /**
  * 配達制御
  */
 void DC_action(void) {
+    if (pre_state != AC_STATUS_DERIVER) {
+        DC_entry();
+        pre_state = AC_STATUS_DERIVER;
+    }
+
+    DC_do_action();
+}
+
+/**
+ * 入口処理
+ */
+static void DC_entry(void) {
+    // 必要に応じて入口処理を実装
+}
+
+/**
+ * 実処理
+ */
+static void DC_do_action(void) {
     // ラインをトレースする
     LDC_do_linetrace();
 
@@ -25,6 +48,7 @@ void DC_action(void) {
     if (!LCU_isLoaded()) {
         // 荷台が押されていない場合
         MU_stop();
+        DC_exit();
         cur_state = AC_STATUS_UNLOADWAIT;
         return;
     }
@@ -32,7 +56,6 @@ void DC_action(void) {
     // 路面の色を示す
     int current_color = LMU_get_color();
     
-
     if (current_color == target_color) {
         // 指定された配達先の色を検知した場合
         MU_stop();
@@ -41,6 +64,14 @@ void DC_action(void) {
         HU_ringArrive();
         
         // 状態を荷降ろし待機に移す
+        DC_exit();
         cur_state = AC_STATUS_DROPBUGGAGE;
     }
+}
+
+/**
+ * 出口処理
+ */
+static void DC_exit(void) {
+    // 必要に応じて出口処理を実装
 } 
